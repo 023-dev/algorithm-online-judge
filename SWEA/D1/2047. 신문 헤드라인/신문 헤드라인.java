cccc8a6a295837fa2ff1b2d2c2ee4a5918bc4882
023 <rhdgmltkd88@gmail.com>
2025-11-14 05:24:23 +0900

    [D1] Title: 신문 헤드라인, Time: 90 ms, Memory: 25,728 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.FileInputStream;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		String T=sc.next();
+        T = T.toUpperCase();
+		System.out.println(T);
+	}
+}
