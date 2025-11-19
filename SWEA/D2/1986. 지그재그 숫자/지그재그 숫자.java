20910af4cf2b340e64fea6a7e676ac377192a4ca
023 <rhdgmltkd88@gmail.com>
2025-11-20 00:30:59 +0900

    [D2] Title: 지그재그 숫자, Time: 103 ms, Memory: 26,368 KB -BaekjoonHub

---
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution{
+	public static void main(String args[]) throws Exception	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)	{
+            int N = sc.nextInt();
+            int result = 0;
+            for (int i = 1; i <= N; i++) {
+                if (i%2==0) {
+                    result -= i;
+                } else {
+                    result += i;
+                }
+            }
+            System.out.println("#" + test_case + " " + result);
+		}
+	}
+}
