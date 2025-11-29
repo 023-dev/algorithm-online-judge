cdfed548c988871972341a0466906c93e3a23b7c
023 <rhdgmltkd88@gmail.com>
2025-11-29 23:52:10 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14620 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        bfs(0, 0);
+        
+        System.out.println(map[N - 1][M - 1]);
+    }
+
+    public static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {x, y});
+        visited[x][y] = true;
+
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
+            int curX = current[0];
+            int curY = current[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = curX + dx[i];
+                int nextY = curY + dy[i];
+
+                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
+                    continue;
+                }
+
+                if (map[nextX][nextY] == 0 || visited[nextX][nextY]) {
+                    continue;
+                }
+
+                q.offer(new int[] {nextX, nextY});
+                visited[nextX][nextY] = true;
+                map[nextX][nextY] = map[curX][curY] + 1;
+            }
+        }
+    }
+}
