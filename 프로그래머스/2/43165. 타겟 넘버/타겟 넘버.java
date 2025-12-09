ae8161a6cac5a50f9d2b7bb7cd1c9a11bc8d0e47
023 <rhdgmltkd88@gmail.com>
2025-12-10 04:39:35 +0900

    [level 2] Title: 타겟 넘버, Time: 8.59 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,23 @@
 class Solution {
+    static int[] numbers;
+    static int target;
+    static int answer = 0;
     public int solution(int[] numbers, int target) {
-        // 깊이(index) 0, 현재 합계(sum) 0에서 시작
-        return dfs(numbers, target, 0, 0);
+        this.numbers = numbers;
+        this.target = target;
+        dfs(0, 0);
+        
+        return answer;
     }
-
-    private int dfs(int[] numbers, int target, int depth, int sum) {
-        // 1. 탈출 조건 (Base Case): 모든 숫자를 다 탐색했을 때
-        if (depth == numbers.length) {
-            // 현재까지의 합이 타겟과 같으면 1(성공), 아니면 0(실패) 반환
-            return sum == target ? 1 : 0;
+    
+    static void dfs(int index, int sum) {
+        if (index == numbers.length) {
+            if (sum == target) {
+                answer++;
+            }
+            return;
         }
-
-        // 2. 수행 동작 (Recursive Step):
-        // 현재 숫자를 '더하는 경우'와 '빼는 경우'를 각각 재귀 호출하고 결과를 합산
-        return dfs(numbers, target, depth + 1, sum + numbers[depth]) 
-             + dfs(numbers, target, depth + 1, sum - numbers[depth]);
+        dfs(index+1, sum + numbers[index]);
+        dfs(index+1, sum - numbers[index]);
     }
 }
