9e0f35161247cb6602729758e36ec1739dfbfff4
023 <rhdgmltkd88@gmail.com>
2025-12-21 00:34:06 +0900

    [Silver II] Title: 나무 자르기, Time: 852 ms, Memory: 123392 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -4,41 +4,38 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
         
-        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        
-        // sort for binary search
         Arrays.sort(arr);
         
-        // binary search
+        long result = 0;
         long left = 0;
         long right = arr[arr.length-1];
-        long result = 0;
-        while(left <= right) {
+        while (left <= right) {
             long mid = (left + right) / 2;
-            
-            long sum = 0;
-            for (int i = 0; i < N; i++) {
-                if (arr[i] > mid) {
-                    sum += (arr[i] - mid);   
+            long sum = 0;            
+            for (long element : arr) {
+                if (element > mid) {
+                    sum += (element - mid);
                 }
             }
             
-            if (sum >= M) {
-                result = mid; 
+            if (sum >= m) {
+                result = mid;
                 left = mid + 1;
-            } else if (sum < M) {
+            } else {
                 right = mid - 1;
             }
         }
+        
         System.out.println(result);
     }
 }
