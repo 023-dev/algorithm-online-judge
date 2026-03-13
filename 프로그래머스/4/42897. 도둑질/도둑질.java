88bdb0461d93476bf49aee600c6c1c8ab7841608
023 <rhdgmltkd88@gmail.com>
2026-03-13 22:48:50 +0900

    [level 4] Title: 도둑질, Time: 7.73 ms, Memory: 97.4 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public int solution(int[] money) {
+        int n = money.length;
+
+        if (n == 1) {
+            return money[0];
+        }
+
+        return Math.max(
+            rob(money, 0, n - 2),
+            rob(money, 1, n - 1)
+        );
+    }
+
+    private int rob(int[] money, int start, int end) {
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int i = start; i <= end; i++) {
+            int current = Math.max(prev1, prev2 + money[i]);
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+}
