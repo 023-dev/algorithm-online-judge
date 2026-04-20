c0ed4416db73cb966ce1f9d4b2788190d0ae91eb
023 <rhdgmltkd88@gmail.com>
2026-04-20 22:49:43 +0900

    [level 2] Title: 주식가격, Time: 42.74 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,28 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        Queue<Integer> q = new LinkedList<>();
-        
-        for (int price : prices) {
-            q.offer(price);
-        }
-        
-        int index = 0;
-        while (!q.isEmpty()) {
-            int curPrice = q.poll();
-            int fall_period = 0;
-            for (int nextPrice : q) {
-                if (nextPrice >= curPrice) {
-                    fall_period++;
-                } else {
-                    fall_period++;
-                    break;
-                }
+        int n = prices.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int topIndex = stack.pop();
+                answer[topIndex] = i - topIndex;
             }
-            answer[index++] = fall_period;
+
+            stack.push(i);
         }
-        
+
+        while (!stack.isEmpty()) {
+            int topIndex = stack.pop();
+            answer[topIndex] = n - 1 - topIndex;
+        }
+
         return answer;
     }
 }
